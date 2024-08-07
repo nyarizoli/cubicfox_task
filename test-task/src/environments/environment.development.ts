31e06555111c03e1825cf1ba628ccac49a8a633d
Zoltan Nyari <zotya19990707@gmail.com>
2024-08-07 13:44:53 +0200

    Add env filée

---
+++ test-task/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  CLIENT_ID: 'INSERT HERE THE ACTUAL CLIENT_ID',
+  CLIENT_SECRET: 'INSERT HERE THE ACTUAL CLIENT_SECRET'
+};
---
+++ test-task/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  CLIENT_ID: 'INSERT HERE THE ACTUAL CLIENT_ID',
+  CLIENT_SECRET: 'INSERT HERE THE ACTUAL CLIENT_SECRET'
+};
